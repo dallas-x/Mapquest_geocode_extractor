789701eee9e5a5801f4641a88160ac5be61b807c
Dallas Baker <baker.dallas.c@outlook.com>
2018-08-13 14:27:46 -0700

    allowed clipboard access

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const electron = require('electron');
 const ipc = electron.ipcMain;
 const app = electron.app;
 const gui = electron.BrowserWindow;
+const Menu = electron.Menu;
 const test = require('./controllers/test');
 // Create and institiate main gui
 let mainGui;
@@ -11,6 +12,27 @@ app.on('ready', _ =>{
       mainGui = null
     });
     mainGui.loadURL(`file://${__dirname}/views/index.html`);
+    // Create the Application's main menu
+    var template = [{
+        label: "Application",
+        submenu: [
+            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
     // When user clicks button {get cordinates} 
     // Send user provided link HTML extractor function 
